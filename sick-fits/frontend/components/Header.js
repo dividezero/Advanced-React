e9d7fc41217b3d844a8fb440a2f2f24777b1e0cc
haro <hazlan@gmail.com>
2020-09-23 14:04:21 +0800

    step8 done; NProgress

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,15 @@
 import Link from "next/link";
 import styled from "styled-components";
 import Nav from "./Nav";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.events.on('routeChangeStart', (url) => {
+    console.log(`Loading: ${url}`)
+    NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const Logo = styled.h1`
   font-size: 4rem;

75b853e251e79f490dd8d9d1a98ec292d8c9054c
haro <hazlan@gmail.com>
2020-09-29 17:29:24 +0800

    step14 done: Query and mutations starter

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    //TODO check if logged in
+    const { itemCreateInput } = args;
+    const newItem = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...itemCreateInput,
+        },
+      },
+      info
+    );
+    return newItem;
+  },
+};
 
 module.exports = Mutations;
